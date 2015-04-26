f7bd9db6d7a8a933ba9fafd5b1c0f3a7d40e1957
Daniel Granger <dg1234uk@me.com>
2015-04-26 20:49:59 +0100

    Object and Function stubs add.

---
+++ snake.js
@@ -0,0 +1,72 @@
+// Constants
+var COLS = 26;
+var ROWS = 26;
+// IDs
+var EMPTY = 0;
+var SNAKE = 1;
+var FRUIT = 2;
+
+var grid = {
+  width : null,
+  height: null,
+  _grid: null,
+
+  init: function(snakeDirection, coloumns, rows) {
+
+  },
+
+  set: function(val, x, y) {
+
+  },
+
+  get: function(x, y) {
+
+  }
+}
+
+var snake = {
+  direction: null,
+  _queue: null,
+
+  init: function(snakeDirection, x, y) {
+
+  },
+
+  insert: function(x, y) {
+
+  },
+
+  remove: function() {
+
+  }
+}
+
+function setFood() {
+
+}
+
+function main() {
+
+}
+
+function init() {
+
+}
+
+function loop() {
+
+}
+
+function update() {
+
+}
+
+function draw() {
+
+}
+
+main();
+
+
+
+
